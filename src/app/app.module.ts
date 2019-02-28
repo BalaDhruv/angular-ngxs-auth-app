88af3b69c1a176230448cc3a45087f66a9340818
BalaDhruv <androbalamail@gmail.com>
2019-03-01 00:26:55 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeLayoutComponent } from './ui/layout/home-layout/home-layout.component';
+import { NavBarComponent } from './ui/layout/nav-bar/nav-bar.component';
+import { SideBarComponent } from './ui/layout/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeLayoutComponent,
+    NavBarComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
